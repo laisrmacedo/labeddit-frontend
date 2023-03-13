cdadc184b3f35b5e1b02935be8717bdabdbb9848
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-13 16:42:37 -0300

    :construction: base file for LoginPage

---
+++ labeddit/src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+import { Footer } from "../components/Footer"
+import { Headers } from "../components/Header"
+
+export const LoginPage = () => {
+  return (
+    <>
+    <Footer/>
+    </>
+  )
+}
