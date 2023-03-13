3f02a57f9f191b35ae4928b4c9c2179d561c5ae8
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-13 19:12:13 -0300

    :lipstick: refactor

--- labeddit/src/App.js
+++ labeddit/src/App.js
@@ -1,13 +1,20 @@
-// import logo from './logo.svg';
-// import './App.css';
-
+import styled from "styled-components";
 import { Router } from "./router/Router";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 
 function App() {
   return (
-    <div className="App">
+    <Container>
      <Router></Router>
-    </div>
+    </Container>
   );
 }
 
