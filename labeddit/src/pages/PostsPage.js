55d8201063bd2fa5255c2f41f1be82bdea2cb42a
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-13 16:42:18 -0300

    :construction: base file for PostsPage

---
+++ labeddit/src/pages/PostsPage.js
@@ -0,0 +1,11 @@
+import { Footer } from "../components/Footer"
+import { Headers } from "../components/Header"
+
+export const PostsPage = () => {
+  return (
+    <>
+    <Headers/>
+    <Footer/>
+    </>
+  )
+}
