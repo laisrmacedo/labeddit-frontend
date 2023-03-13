e4d9fed91babfc9aeb0861cb9cd1374ce4a730ef
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-13 16:14:08 -0300

    :sparkles: feat: global style

---
+++ labeddit/src/GlobalStyle.js
@@ -0,0 +1,26 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    overflow-x: hidden;
+    font-weight: 600;
+    font-family: 'Poppins', sans-serif;
+    color: white;
+    ::-webkit-scrollbar-track {
+    background: #101010; 
+    };
+    ::-webkit-scrollbar{
+    width: 8px;
+    };
+    ::-webkit-scrollbar-thumb {
+    background: #505050; 
+    border-radius: 10px;
+    };
+    ::-webkit-scrollbar-thumb:hover {
+    background: gray; 
+    }
+  }
+`
--- labeddit/src/index.js
+++ labeddit/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App';
+import { GlobalStyle } from './GlobalStyle';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle/>
     <App />
   </React.StrictMode>
 );
