52f9038b48c409d4b703c200bfc7564e3619709e
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-13 19:10:55 -0300

    :lipstick: update: format and font size

--- labeddit/src/components/Footer.js
+++ labeddit/src/components/Footer.js
@@ -3,65 +3,62 @@ import linkedin from '../assets/linkedin.png'
 import github from '../assets/github.png'
 
 const Container = styled.div`
-  height: 50px;
-  width: 100vw;
-  /* background-color: #5D5D5D; */
+  height: 6%;
+  width: 100%;
   display: flex;
   align-items: flex-end;
   justify-content: center;
   bottom: 0px;
   
-  border: 1px red solid;
   div{
+    /* border: 1px red solid; */
     height: 100%;
-    max-width: 1275px;
+    width: 364px;
     display: flex;
     align-items: center;
-    justify-content: center;
-    gap: 16px;
+    justify-content: space-between;
   }
   p{
     font-weight: 100;
-    font-size: 12px;
+    font-size: 8px;
     text-transform: uppercase;
     color: #C4C4C4;
   }
   span{
     display: flex;
     align-items: center;
-    gap: 5px;
+    gap: 8px;
     a{
       display: flex;
       align-items: center;
     }
   }
   img{
-    width: 20px;
-    filter: invert();
-    &:hover{
-      filter: none;
-    }
+    width: 16px;
+    filter: brightness(2);
     &:active{
       filter: brightness(1.5);
     }
   }
   `
 
-export const Footer = () => { 
+export const Footer = () => {
 
-  return(
+  return (
     <Container>
       <div>
+        <span>
         <p>Designed by LABENU</p>
         <p>|</p>
+        <p>Built by Laís Macedo</p>
+        </span>
         <span>
-          <p>Built by Laís Macedo</p>
-            <a href="https://www.linkedin.com/in/laisrmacedo/" target="_blank">
-                <img src={linkedin} alt=""/>
-            </a>
-            <a href="https://github.com/laisrmacedo" target="_blank">
-                <img src={github} alt=""/>
-            </a>
+          <a href="https://www.linkedin.com/in/laisrmacedo/" target="_blank">
+            <img src={linkedin} alt="" />
+          </a>
+          <a href="https://github.com/laisrmacedo" target="_blank">
+            <img src={github} alt="" />
+          </a>
         </span>
       </div>
     </Container>
