30c469f2a9ecf77338323dfc7930dc33620b0bc9
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-15 10:08:30 -0300

    :sparkles: feat: comments page route

--- labeddit/src/router/Router.js
+++ labeddit/src/router/Router.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CommentsPage } from "../pages/CommentsPage";
 import { LoginPage } from "../pages/LoginPage";
 import { PostsPage } from "../pages/PostsPage";
 import { SignupPage } from "../pages/SignupPage";
@@ -10,6 +11,7 @@ export const Router = () => {
         <Route path={`/users/login`} element={<LoginPage/>}/>
         <Route path={`/users/signup`} element={<SignupPage/>}/>
         <Route path={`/posts`} element={<PostsPage/>}/>
+        <Route path={`/posts/:id`} element={<CommentsPage/>}/>
       </Routes>
     </BrowserRouter>
   )
