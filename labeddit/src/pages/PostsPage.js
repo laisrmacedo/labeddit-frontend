b9e3a8cbfdd9378d670b9d742d1d2a048cfb81f2
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-15 17:09:48 -0300

    :sparkles: feat: frontend and backend integration

--- labeddit/src/pages/PostsPage.js
+++ labeddit/src/pages/PostsPage.js
@@ -1,55 +1,71 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { BASE_URL } from "../App";
 import { Footer } from "../components/Footer"
 import { Headers } from "../components/Header"
 import { PostComment } from "../components/PostComment";
-import { HorizontalLine, InputForLongText, Radius8Btn } from "../components/styledcomponents";
-
-const Container = styled.div`
-  height:calc(94% - 50px - 44px);
-  width: 364px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 32px 0 0 0;
-  /* justify-content: center; */
-  /* border: 2px solid blue; */
-  input{
-    height: 131px;
-    width: 100%;
-   padding: 18px 0 90px 18px;
-      border: 1px solid #D5D8DE;
-      border-radius: 8px;
-      background-color: #EDEDED;
-      margin-bottom: 12px;
-  }
-  >span{
-    width: 100%;
-    height: 1px;
-    background: linear-gradient(90deg, #FF6489, #F9B24E);
-    margin: 16px 0 36px 0;
-  }
-  button{
-    width:100%;
-    height: 51px;
-    border-radius: 8px;
-    border: none;
-    background: linear-gradient(90deg, #FF6489, #F9B24E);
-  }
-`
+import { Box, Container, HorizontalLine, InputForLongText, Radius8Btn } from "../components/styledcomponents";
 
 export const PostsPage = () => {
+  const [allPosts, setAllPosts] = useState([])
+  const [post, setPost] = useState("")
+
+  const onChangePost = (e) => {
+    setPost(e.target.value)
+  }
+
+  const headers = {
+    headers: {
+      authorization: localStorage.getItem("token")
+    }
+  }
+
+  const body = {
+    content: post
+  }
+
+  const createPost = async () => {
+    try {
+      await axios.post(BASE_URL + `posts`, body, headers)
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get(BASE_URL + `posts`, headers)
+      setAllPosts(response.data)
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [allPosts])
+
   return (
     <>
       <Headers
         isPostsPage={true}
       />
       <Container>
-        <InputForLongText placeholder="Escreva seu post..." />
-        <Radius8Btn>Postar</Radius8Btn>
+        <InputForLongText 
+          placeholder="Escreva seu post..."
+          type="text"
+          name="post"
+          value={post}
+          onChange={onChangePost}
+        />
+        <Radius8Btn onClick={() => createPost()}>Postar</Radius8Btn>
         <HorizontalLine />
-        <div>
-          <PostComment />
-        </div>
+        <Box>
+          {allPosts.map((post) => {
+            return <PostComment key={post.id} post={post} />
+          })}
+        </Box>
       </Container>
       <Footer />
     </>
