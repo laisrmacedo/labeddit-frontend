57532bb7ab6982d808cb92e129b0a167b60f2e3a
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-16 00:57:07 -0300

    :sparkles: feat: conditional renderization

--- labeddit/src/components/PostComment.js
+++ labeddit/src/components/PostComment.js
@@ -16,80 +16,112 @@ const Container = styled.div`
   padding: 9px 10px;
   border-radius: 8px;
   display: flex;
-  flex-direction: column;
-  align-items: start;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: start;
   gap: 18px;
+
+  div{
+    width: 100%;
+    min-height: 56px;
+    display: flex;
+    flex-direction: column;
+    gap: 18px;
+  }
+
   .user{
     color: #6F6F6F;
     font-size: 12px;
   }
+
   .content{
     color: #000;
     font-size: 18px;
   }
-  div{
-    display: flex;
-    gap: 10px;
-    p{
-      color: #6F6F6F;
-      font-size: 12px;
-    }
-    span{
-      display: flex;
-      padding: 5px;
-      border: 1px solid #ECECEC;
-      background-color: #FBFBFB;
-      border-radius: 14px;
-      height: 28px;
-    }
-    .votes{
-      justify-content: space-between;
-    width: 98px;
-    }
-    .comments{
-      justify-content: space-around;
 
+  p{
+    color: #6F6F6F;
+    font-size: 12px;
+  }
+
+  span{
+    display: flex;
+    padding: 5px;
+    border: 1px solid #ECECEC;
+    background-color: #FBFBFB;
+    border-radius: 14px;
+    height: 28px;
+  }
+
+  .votes{
+    justify-content: space-between;
+    width: 98px;
+  }
+
+  .comments{
+    justify-content: space-around;
     width: 65px;
-    }
   }
 `
 export const PostComment = (props) => {
   const navigate = useNavigate()
-  const [post, setPost] = useState({})
+  const [content, setContent] = useState({})
 
   const headers = {
     headers: {
       authorization: localStorage.getItem("token")
     }
   }
-    const getPostById = async () => {
+
+  const getPostById = async (id) => {
     try {
-      const response = await axios.get(BASE_URL + `posts/${props.post.id}`, headers)
-      setPost(response.data)
+      const response = await axios.get(BASE_URL + `posts/${id}`, headers)
+      setContent(response.data)
+      props.setPostComments(response.data.comments)
     } catch (error) {
       console.log(error.response.data)
     }
   }
 
   useEffect(() => {
-    getPostById()
+    getPostById(props.postId)
   }, [])
 
   return (
     <Container>
-      <p className="user">Enviado por: {post.creatorNickname}</p>
-      <p className="content">{post.content}</p>
-      <div>
-        <span className="votes">
-          <img src={upvote} />
-          <p>{post.upvote}</p>
-          <img src={downvote} />
-        </span>
-        <span className="comments" onClick={() => goToCommentsPage(navigate, post.id)}>
+      {props.isPost ?
+        <>
+          <div>
+            <p className="user">Enviado por: {content.creatorNickname}</p>
+            <p className="content">{content.content}</p>
+          </div>
+          <span className="votes">
+            <img src={upvote} />
+            <p>{content.upvote}</p>
+            <img src={downvote} />
+          </span>
+        </>
+        :
+        <>
+          <div>
+            <p className="user">Enviado por: {props.commentNickname}</p>
+            <p className="content">{props.commentContent}</p>
+          </div>
+          <span className="votes">
+            <img src={upvote} />
+            <p>{props.commentUpvote}</p>
+            <img src={downvote} />
+          </span>
+
+        </>
+
+      }
+      {props.isPost &&
+        <span className="comments" onClick={() => goToCommentsPage(navigate, content.id)}>
           <img src={comments} />
-          <p>{post.comments?.length}</p>
+          <p>{content.comments?.length}</p>
         </span>
-      </div>
+      }
     </Container>
   )
 }
