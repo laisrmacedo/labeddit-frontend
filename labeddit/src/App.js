5a1d4670cefccb9d5cc881b179fa383cd8a88085
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-18 07:12:58 -0300

    :bug: fix: BASE_URL

--- labeddit/src/App.js
+++ labeddit/src/App.js
@@ -1,7 +1,7 @@
 import { Body } from "./GlobalStyle";
 import { Router } from "./router/Router";
 
-export const BASE_URL = 'https://labeddit-backend-deploy.onrender.com'
+export const BASE_URL = 'https://labeddit-backend-deploy.onrender.com/'
 
 function App() {
   return (
