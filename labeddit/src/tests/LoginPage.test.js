fb6496c74c78c26b2e815e95e1170df52db1877a
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-31 14:40:48 -0300

    :sparkles: feat: frontend test

--- labeddit/src/tests/LoginPage.test.js
+++ labeddit/src/tests/LoginPage.test.js
@@ -4,24 +4,44 @@ import { LoginPage }  from '../pages/LoginPage'
 // import userEvent from "@testing-library/user-event"
 // import logoLogin from '../assets/logoLogin.png'
 import axios from "axios"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+// import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Router } from "../router/Router"
+import { createMemoryHistory } from 'history'
 
-jest.mock("axios")
+// jest.mock("axios")
+
+const renderWithRouter = (component) => {
+    const history = createMemoryHistory()
+    return { 
+    ...render (
+    <Router history={history}>
+        {component}
+    </Router>
+    )
+  }
+}
 
 describe("LoginPage", () => {
     it("render image, title and subtitle", async () => {
+        renderWithRouter(<LoginPage/>)
+        screen.logTestingPlaygroundURL()
+
       // axios.get.mockResolvedValueOnce()
       // const user = userEvent.setup()
-      render(
-        <BrowserRouter>
-            <Routes>   
-                <Route path={`/users/login`} element= {<LoginPage/>}/>
-            </Routes>
-        </BrowserRouter>)
 
-        expect(screen.getByText(/O projeto de rede social da Labenu/i)).toBeInTheDocument()
+    //   render(
+    //     <BrowserRouter>
+    //         <Routes>
+    //             <Route path={`/users/login`} element={<LoginPage/>}/>
+    //         </Routes>
+    //     </BrowserRouter>)
+
+    //     screen.debug()
+
+    // render(<Router/>)
+
+        // expect(screen.getByText(/O projeto de rede social da Labenu/i)).toBeInTheDocument()
         // await waitFor(() => { })
-        screen.logTestingPlaygroundURL()
 
         // expect(title).toBeInTheDocument()
     })
