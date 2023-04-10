132d107e11d202d5cff738d6d477e6f41bc2ea80
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-04-10 16:56:58 -0300

    :recycle: refactor: link to backend deploy

--- labeddit/src/App.js
+++ labeddit/src/App.js
@@ -1,8 +1,8 @@
 import { Body } from "./GlobalStyle";
 import { Router } from "./router/Router";
 
-export const BASE_URL = 'http://localhost:3003/'
-// export const BASE_URL = 'https://labeddit-backend-deploy.onrender.com/'
+// export const BASE_URL = 'http://localhost:3003/'
+export const BASE_URL = 'https://labeddit-backend-deploy.onrender.com/'
 
 function App() {
   return (
